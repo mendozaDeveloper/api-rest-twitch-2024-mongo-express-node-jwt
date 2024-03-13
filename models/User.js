0c63af2cf0dbe2e76a5a99d9875dfed528cd12dc
LuisMendoza33 <lmendozaguerra@gmail.com>
2024-03-13 17:48:59 -0500

    insertando al usuario en la bd, (controllers, models,middlewares, routes)

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcryptjs from "bcryptjs"
 
 const {Schema, model} = mongoose;
 
@@ -16,6 +17,27 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// Esto hashea la contraseña antes de guardarla con el metodo SAVE
+userSchema.pre("save", async function(next){
+    const user = this;
+
+    if(!user.isModified("password")) return next()
+
+    try {
+        const salt = await bcryptjs.genSalt(10)
+        user.password = await bcryptjs.hash(user.password, salt)
+        next()
+    } catch (error) {
+        console.log(error)
+        throw new Error("Fallo el hash de la contraseña")
+    }
+})
+
+//metodo para comparar la contraseña guardada en BD con la que manda el usuario al momento de logearse
+userSchema.methods.comparePassword = async function(candidatePassword){
+    return await bcryptjs.compare(candidatePassword, this.password)
+}
 
 export const User = model('user', userSchema)
 
