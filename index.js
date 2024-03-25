0b403bff508082aace757a110f277b167d32f6a0
LuisMendoza33 <lmendozaguerra@gmail.com>
2024-03-24 22:13:34 -0500

    fin ??

--- index.js
+++ index.js
@@ -2,12 +2,35 @@ import 'dotenv/config'
 import './database/connectdb.js'
 import cookieParser from "cookie-parser";
 import express from "express"
+import cors from "cors"
+
 import authRouter from './routes/auth.route.js'
 import linkRouter from './routes/link.route.js'
+import redirectRouter from './routes/redirect.route.js'
 
 const app = express()
+
+//con esta tecnica no entran a los controladores
+const whiteList = [process.env.ORIGIN1]
+
+app.use(cors({
+    origin: function(origin, callback){
+        if(whiteList.includes(origin)) {
+            return callback(null, origin)
+        }
+        return callback(
+            "Erro de CORS origin " + origin + " No autorizado"
+        )
+    }
+}))
+
 app.use(express.json())
 app.use(cookieParser());
+
+// ejemplo back redirect (opcional)
+app.use("/", redirectRouter)
+
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/links", linkRouter);
 
--- routes/link.route.js
+++ routes/link.route.js
@@ -1,12 +1,17 @@
 import { Router } from "express"
-import { createLink, getLinks, getLink, removeLink } from "../controllers/link.controller.js" 
+import { createLink, getLinks, getLink, removeLink, updateLink } from "../controllers/link.controller.js" 
 import { requireToken } from "../middlewares/requireToken.js";
 import { bodyLinkValidator, paramLinkValidator } from "../middlewares/ValidatorManager.js";
 const router = Router()  
 
 router.get("/", requireToken, getLinks)
-router.get("/:id", requireToken, getLink)
+
+router.get("/:nanoLink", getLink)
+
+//router.get("/:id", requireToken, getLink)
+
 router.post("/", requireToken, bodyLinkValidator, createLink);
 router.delete("/:id", requireToken, paramLinkValidator, removeLink);
+router.patch("/:id", requireToken, paramLinkValidator, bodyLinkValidator, updateLink);
 
 export default router;
